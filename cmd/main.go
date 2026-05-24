dc99a3020439111fd12818d69988d836ff4a89b2
Sai Praneeth <saipraneeth0448@gmail.com>
2026-05-24 11:33:24 +0530

    added basic auth with standard user name password

--- cmd/main.go
+++ cmd/main.go
@@ -8,9 +8,30 @@ import (
 func main() {
 
 	port := ":8080"
-	http.HandleFunc("/start-job", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "job initiated")
-	})
+	protected := authMiddleware(adminHandler)
+	http.HandleFunc("/start-job", protected)
 
 	http.ListenAndServe(port, nil)
+}
+
+func authMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		handleBasicAuth(next, w, r)
+	}
+}
+
+func handleBasicAuth(next http.HandlerFunc, w http.ResponseWriter, r *http.Request) {
+	user, pass, ok := r.BasicAuth()
+
+	if !ok || user != "admin" || pass != "password123" {
+		w.Header().Set("WWW-Authenticate", `Basic realm = Restricted`)
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	next(w, r)
+}
+
+func adminHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "handled the auth")
 }
